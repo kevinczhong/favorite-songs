7411d046130eaae9f728b5ab7cb803a5b5944f4b
Kevin Zhong <kvnzhong@gmail.com>
2022-05-05 11:32:16 -0700

    Added provisional update method; not yet tested.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,20 @@
 class SongsController < ApplicationController
+  def index
+    songs = Song.all
+    render json: { songs: songs }
+  end
+
+  def show
+    song = Song.find_by(id: params[:id])
+    render json: { song: song }
+  end
+
+  def create
+    song = Song.create(
+      title: params["title"],
+      album: params["album"],
+      artist: params["artist"],
+      year: params["year"]
+    )
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  get "/songs" => "songs#index"
+  get "/songs/:id" => "songs#show"
+  post "/songs" => "songs#create"
 end
