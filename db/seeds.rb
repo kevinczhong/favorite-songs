29f3d64d84673c96e15da2fd3b2867af4a5c3af9
Kevin Zhong <kvnzhong@gmail.com>
2022-05-05 11:22:50 -0700

    Generated model, controller.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Song.create(title: "Karma Police", album: "Okay Computer", artist: "Radiohead", year: "1997")
+
+Song.create(title: "Giving You Up", album: "Steady Heart", artist: "Kameron Marlowe", year: "2022")
+
+Song.create(title: "Star Spangled Banner", album: "Independence Songs", artist: "US Band", year: "1814")
